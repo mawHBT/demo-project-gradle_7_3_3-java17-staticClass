b0c8e05ae3b230cb0d269e30e03ceef42c3c745b
Matti Wilhelmi <maw@hbt.de>
2022-05-25 13:28:50 +0200

    Added constructor for StaticClazz

--- src/main/java/demo/project/gradle/ClazzWithStaticClazz.java
+++ src/main/java/demo/project/gradle/ClazzWithStaticClazz.java
@@ -1,10 +1,17 @@
 package demo.project.gradle;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ClazzWithStaticClazz {
 
    public static class StaticClazz implements StaticInterface, Serializable {
+
+      private ArrayList<StaticInterface> staticInterfaceList;
+
+      public StaticClazz() {
+         staticInterfaceList = new ArrayList<StaticInterface>();
+      }
 
       @Override public boolean staticInterfaceMethod(boolean someBoolean) {
          return !someBoolean;
